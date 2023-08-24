dc489ff1152e549e1bb7e59d41dbb36529cc6a94
Trung Nguyen <>
2023-08-24 19:05:52 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,11 +22,6 @@ export default function Home() {
   return (
     <AppContext.Provider value={{chatItems, setChatItems}}>
       <main className="p-10">
-        <div className="text-center hidden">
-          <h2 className="mb-10">What is the tallest forest in the world?</h2>
-          <Link href="/answer"><span className="px-2 py-4 w-40 inline-block bg-white rounded text-black">OK</span></Link>
-        </div>
-
         <div className="grid grid-cols-5 gap-10">
           <div className="col-span-2">
             <BoxInput />
